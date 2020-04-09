37cb12793249c7af8dbb89a6424696b71d0173b2
ounula <379923145@qq.com>
2020-04-09 13:28:17 +0800

    1

--- lemon_web_02.py
+++ lemon_web_02.py
@@ -4,10 +4,11 @@
 from selenium import webdriver
 from  selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from  selenium.webdriver.common.action_chains import ActionChains
 from  selenium.webdriver.common.by import By
 #导入select 库
 from selenium.webdriver.support.ui import Select
+#导入鼠标操作库
+from  selenium.webdriver.common.action_chains import ActionChains
 driver=webdriver.Chrome()
 driver.maximize_window()
 driver.get("http://www.baidu.com")
@@ -24,5 +25,15 @@ ele = driver.find_element_by_xpath('//following::div[@id="u1"]/a[@class ="pf"]')
 ActionChains(driver).move_to_element(ele).perform()
 
 WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH,'//a[text()="高级搜索"]')))
-driver.find_element_by_xpath('//a[text()="高级搜索"]').click()ol
+driver.find_element_by_xpath('//a[text()="高级搜索"]').click()
+
+se_ele=driver.find_element_by_xpath('//select[@name="ft"]')
+sele=Select(se_ele)
+#通过文字
+sele.select_by_visible_text('微软 Word (.doc)')
+#通过value值
+sele.select_by_value('rtf')
+#通过下标
+sele.select_by_index('4')
+
 
